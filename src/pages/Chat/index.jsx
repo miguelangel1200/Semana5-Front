f9b33091558473c36cecf25775bb5fa536540060
Miguel Angel <miguel@gmail.com>
2022-09-23 12:26:40 -0500

    first commit

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from "react";
 import { ModalRegister, DrawerList } from "../../components";
+import { get } from "../../service"
+import Pusher from "pusher-js";
 
 const Chat = () => {
+
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = async () => {
+    const response = await get("/user");
+    console.log(response)
+    setUsers(response.data)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('67479aa6e2e3a1fa10f5', {
+      cluster: "us2"
+    })
+
+    const channel = pusher.subscribe("my-chat")
+    channel.bind("my-list-contacs", async ({ message }) => {
+      console.log("message from pusher", message)
+    }) 
+  }, [])
+
+
   return (
     <div>
-      <DrawerList />
-      <ModalRegister />
+      {users.length > 0 && <DrawerList users={users} />}
+      <ModalRegister fetchUsers={fetchUsers}/>
     </div>
   );
 };
